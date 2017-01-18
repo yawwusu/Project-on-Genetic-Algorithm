ab227065bfb5613fd6fa51d9a08a5f6cb0d68faa
Yaw Owusu-Agyemang <yawwusugh@gmail.com>
2017-01-18 18:37:41 +0000

    Update Test_1.py
    changing names and arguments to suit modifications in All_Necessary_Funcs.py

--- Test_1.py
+++ Test_1.py
@@ -20,12 +20,13 @@ while index < 100:       #stopping criteria 100 for now
     eval_list = evaluate(initial_pop, 'c.csv', 'x3.csv','d.csv')
     
     #Selection
-    mate_pool = selection(initial_pop, eval_list, 5)  # 2 offspring in the pool
+    mate_pool = RouletteWheelSelection(initial_pop, eval_list, 5)  # 2 offspring in the pool
+    
     #Crossover
     parents,offspring = Uniform_crossover(mate_pool, 0.8)   #Pc = 0.8
+    
     #Mutate
-    #problem here,  ##genes can repeat
-    mutant = mutate(winning_offspring,0.2)    #Pm = 0.2
+    mutant = mutate(offspring,0.2)    #Pm = 0.2
     
     #Elitism
     initial_pop = WeakParent_elitism(initial_pop,parents,mutant)
