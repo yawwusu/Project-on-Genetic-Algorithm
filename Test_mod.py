4045b4e5adac6783cded3e40e86610f8afd6ace2
Yaw Owusu-Agyemang <yawwusugh@gmail.com>
2017-02-21 19:53:29 +0000

    Add files via upload
    some changes in these

---
+++ Test_mod.py
@@ -0,0 +1,32 @@
+from All_Necessary_Funcs import *
+import random
+#Encoding
+Population = encode([1,8,9,11,13,16,19],4)
+
+random.seed(1)  #Check to ensure reproducibility. ##Comment out afterwards
+              
+#Initialization
+initial_pop = initialize(Population,10)
+
+index = 0
+while index < 100:       #stopping criteria 100 for now
+    
+    #Evaluation
+    eval_list = evaluate(initial_pop)
+    
+    #Selection
+    mate_pool = Roulette(initial_pop, eval_list, 2)  # 2 offspring in the pool
+
+    #Crossover      #problem here. genes can repeat
+    parents,offspring = NewCrossover(mate_pool, 0.8)   #Pc = 0.8
+
+    #Mutate
+    mutant = mutate(offspring,0.2)    #Pm = 0.2
+    
+    #Elitism        #problem here. change in corresponding with crossover and make same input as Random
+    initial_pop = Random(initial_pop,mutant)
+    
+    index += 1
+
+print index,len(initial_pop)
+print max_occurs(initial_pop)
---
+++ genetic_mod_func.py
@@ -0,0 +1,34 @@
+from All_Necessary_Funcs import *
+def genetic(Pop_Size = 50 , Pc = 0.8, Pm = 0.2, max_iter = 100, selection = Roulette, crossover = NewCrossover,elitism = Random):
+    #Encoding
+    Population = encode([1,8,9,11,13,16,19],4)
+                    
+    #Initialization
+    random.seed(1)
+    initial_pop = initialize(Population,Pop_Size)
+    
+    
+    index = 0
+    while index < max_iter:       #stopping criteria 100 for now
+        
+        #Evaluation
+        eval_list = evaluate(initial_pop)
+        
+        #Selection
+        mate_pool = selection(initial_pop, eval_list, 5)  # 2 offspring in the pool
+        
+        #Crossover
+        parents,offspring = crossover(mate_pool, Pc)   #
+        
+        #Mutate
+        #problem here,  ##genes can repeat
+        mutant = mutate(offspring,Pm)    #
+        
+        #Elitism
+        initial_pop = elitism(initial_pop,mutant)   
+        # form of elitism ##change
+        
+        index += 1
+    
+    print 'There are ', len(initial_pop), 'chromosomes in the final generation. The best is:'
+    return max_occurs(initial_pop)
